832112043c2a28cb7894b2714ac1846a0b9d81c4
Cinda <cindarella0@hotmail.com>
2021-03-15 10:35:06 +0100

    Schaltjahr

--- src/Schaltjahr.java
+++ src/Schaltjahr.java
@@ -3,7 +3,20 @@ public class Schaltjahr {
     public static void main(String[] args) {
         int year = 0;
 
-        System.out.println(isLeapYear());
+        isLeapYear(1992);
+        isLeapYear(2000);
+        isLeapYear(1991);
+
+        System.out.println(allLeapYear(0));
+
+        /*int count = 0;
+        for (int i = 0; i<= 2100; i++) {
+            if (isLeapYear(i)) {
+                count++;
+            }
+        }
+
+         */
 
 
 
@@ -11,7 +24,33 @@ public class Schaltjahr {
 
     }
 
-    public static int isLeapYear (int year) {
+
+    public static boolean istSchaltjahr (int year, boolean showPrintMessage) {
+        if (year% 100 == 0) {
+            if ( year % 400 == 0) {
+                System.out.println(year + " is a Leap Year");
+            }
+        }
+        return year;
+    }
+
+    public static void isLeapYear (int year) {
+
+        if (year% 100 == 0) {
+            if ( year % 400 == 0) {
+                System.out.println(year + " is a Leap Year");
+            }
+        }
+        else
+            if (year % 4 == 0) {
+            System.out.println(year + " is a Leap year");
+        }
+            else {
+                System.out.println(year + " is not Leap year");
+            }
+    }
+
+    public static int allLeapYear (int year) {
         int counter = 0;
         for (int i = 1;i <= 2100;i++){
             if (i % 4 == 0) {
